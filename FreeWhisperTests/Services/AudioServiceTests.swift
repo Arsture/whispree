4a4c6ff4bc6d3a40d76ce44dccbd41e6ac77bb30
Arsture <kms23806512@gmail.com>
2026-03-06 10:37:23 +0900

    test: add audio service unit tests

---
+++ FreeWhisperTests/Services/AudioServiceTests.swift
@@ -0,0 +1,9 @@
+import XCTest
+@testable import FreeWhisper
+
+final class AudioServiceTests: XCTestCase {
+    // Placeholder - tests will be implemented in Phase 9
+    func testPlaceholder() {
+        XCTAssertTrue(true)
+    }
+}
